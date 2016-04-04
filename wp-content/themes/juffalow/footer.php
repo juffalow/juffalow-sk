6cc837cf7cb4ca027a8f81fba987a3a825a5fd5c
Matej Jellus <juffalow@juffalow.com>
2016-04-04 23:23:06 +0200

    footer change

--- wp-content/themes/juffalow/footer.php
+++ wp-content/themes/juffalow/footer.php
@@ -2,12 +2,18 @@
 
     <footer class="footer">
         <div class="container">
-            <span>Matej <small>'juffalow'</small> Jellus  |  <i class="fa fa-wordpress"></i>ordPress  |  2015</span>
-            <span class="social">
-                <a href="https://www.facebook.com/juffalow.page"><i class="fa fa-facebook"></i></a>
-                <a href="https://twitter.com/juffalow"><i class="fa fa-twitter"></i></a>
-                <a href="https://github.com/juffalow"><i class="fa fa-github"></i></a>
-            </span>
+            <div class="row">
+                <div class="col-sm-6 col-xs-12">
+                    <span>Matej <small>'juffalow'</small> Jellus  |  <i class="fa fa-wordpress"></i>ordPress  |  2015</span>
+                </div>
+                <div class="col-sm-6 col-xs-12">
+                    <span class="social">
+                        <a href="https://www.facebook.com/juffalow.page"><i class="fa fa-facebook"></i></a>
+                        <a href="https://twitter.com/juffalow"><i class="fa fa-twitter"></i></a>
+                        <a href="https://github.com/juffalow"><i class="fa fa-github"></i></a>
+                    </span>
+                </div>
+            </div>
         </div>
     </footer>
     </body>
